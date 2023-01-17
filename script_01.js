fd1849acdcc617db4550741ab9b4e66cd6166b51
Gazelle2010 <martin-schwerdtner@t-online.de>
2023-01-17 11:49:37 +0100

    Funktion 02 a + b

--- script_01.js
+++ script_01.js
@@ -1,6 +1,42 @@
 "use strict";
 
 /***** Funktionen 01 *****/
-
 // 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+// function test()
+// {
+//     console.log("Hallo Martin!");
+// }
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// ausgabeNamen();
+// console.log(firstName); // Fehler: Scope!
+
+function ausgabeNamen() 
+{
+    let firstName = "Luca"; // what happens in Vegas, ....
+    // console.log("Hallo Martin!");
+    // console.log("Hallo Yunaisy!");
+    console.log("Hallo " + firstName + "!");
+} 
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamenParam("Martin"); // Call + Argumente(e) 
+ausgabeNamenParam("Lawrence");
+// Argumente sind Daten für parameter
+
+function ausgabeNamenParam(firstName) 
+{
+    console.log("Hallo " + firstName + "!");
+} 
+
 
